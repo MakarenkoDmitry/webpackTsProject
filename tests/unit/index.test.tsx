5cb91cfb46830867338f0b092ccd8d5244a69d23
MakarenkoDmitry <makarenkodkh@gmail.com>
2019-01-16 15:45:53 +0200

    buildV1

--- tests/unit/index.test.tsx
+++ tests/unit/index.test.tsx
@@ -1,7 +1,26 @@
+import * as React from 'react';
 import { expect } from 'chai';
+
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+class Foo extends React.Component<any, any> {
+    render() {
+        return <div>Foo</div>;
+    }
+}
+
+class MyComponent extends React.Component<any, any> {
+    render() {
+        return <div>Hello, {this.props.whoToGreet} <Foo/></div>;
+    }
+}
 
 describe('App component testing', function() {
     it('renders welcome message', function() {
-        expect(1).to.equal(1);
+        const wrapper = Enzyme.shallow(<MyComponent />);
+        expect(wrapper.find(Foo)).to.have.lengthOf(1);
     });
 });
